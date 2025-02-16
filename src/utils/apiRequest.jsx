71209e781f5fb074a332b8205df5982ba49382c1
ahtisham02 <ahtisham02>
2025-02-16 16:45:17 +0500

    changes

--- src/utils/apiRequest.jsx
+++ src/utils/apiRequest.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const apiRequest = async (method, url, data = {}, token, headers = {}) => {
   const config = {
     method: method.toLowerCase(), // Normalize method to lowercase
-    url: `http://172.31.26.25:8080${url}`, // Correct URL concatenation
+    url: `http://16.16.88.8:8080${url}`, // Correct URL concatenation
     headers: {
       Authorization: `Bearer ${token}`, // Fixed template literal syntax
       ...headers,
