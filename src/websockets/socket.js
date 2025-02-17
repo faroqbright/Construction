7fe37199ef3569d2ee94262a00f0026e24bdec3a
ahtisham02 <ahtisham02>
2025-02-17 11:58:53 +0500

    chnges

--- src/websockets/socket.js
+++ src/websockets/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 // const socket = io("http://localhost:1000"); // Replace with your backend URL
-const socket = io("http://appsoapro.techbytech.tech:8081"); // Replace with your backend URL
+const socket = io("https://appsoapro.techbytech.tech/api/api/v1"); // Replace with your backend URL
 socket.on("connect", () => {
     console.log("Socket connected to server");
   });
