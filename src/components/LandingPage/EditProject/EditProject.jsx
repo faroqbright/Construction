b462d44bc882b207a5040b3d703a57ae443d0d25
ahtisham02 <ahtisham02>
2025-02-18 16:14:30 +0500

    chnges

--- src/components/LandingPage/EditProject/EditProject.jsx
+++ src/components/LandingPage/EditProject/EditProject.jsx
@@ -163,6 +163,10 @@ export default function EditProject() {
       data.append("physicalEducationRange", "100");
       data.append("daysLeft", "Awaiting Start");
 
+      if (selectedFile) {
+        data.append("attachment", selectedFile);
+      }
+      
       requestData = data;
     } else {
       const updatedFields = {};
@@ -514,6 +518,32 @@ export default function EditProject() {
                 </span>
               )}
             </div>
+            <div className="mb-4">
+              <label className="block text-sm font-semibold mb-2 text-gray-700">
+                {t("Attachment")}
+              </label>
+              <Controller
+                name="attachment"
+                control={control}
+                render={({ field }) => (
+                  <input
+                    type="file"
+                    accept=".pdf, .doc, .docx, .jpg, .png, .jpeg, .gif, .bmp, .webp"
+                    onChange={(e) => {
+                      handleFileChange(e);
+                      field.onChange(e.target.files[0]);
+                    }}
+                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none"
+                  />
+                )}
+              />
+              {errors.attachment && (
+                <span className="text-red-600">
+                  {errors.attachment.message}
+                </span>
+              )}
+            </div>
+
             <Controller
               name="projectOwnerId"
               control={control}
