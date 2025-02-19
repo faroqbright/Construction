84e69ee56e0a840a39997f2fb31edab8c2816ade
ahtisham02 <ahtisham02>
2025-02-19 14:50:09 +0500

    chnges

--- src/components/LandingPage/EditProject/EditProject.jsx
+++ src/components/LandingPage/EditProject/EditProject.jsx
@@ -81,17 +81,20 @@ export default function EditProject() {
     if (file) {
       // Validate file type (for example, allow only image files)
       if (
-        file.type === "image/png" ||
-        file.type === "image/jpeg" ||
-        file.type === "image/gif" ||
-        file.type === "image/bmp" ||
-        file.type === "image/webp"
+        file.type === "application/pdf" || // PDFs
+        file.type === "application/msword" || // Word Documents
+        file.type === "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || // Word (.docx)
+        file.type === "application/vnd.ms-excel" || // Excel (.xls)
+        file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || // Excel (.xlsx)
+        file.type === "text/plain" || // Text files
+        file.type === "application/zip" || // Zip files
+        file.type === "application/x-rar-compressed"
       ) {
         // File is valid
         setSelectedFil(file);
       } else {
         // Invalid file type, show an error
-        toast.error("Please select a valid image file.");
+        toast.error("Please select a valid file.");
         e.target.value = null; // Clear the input field
       }
     }
@@ -185,10 +188,6 @@ export default function EditProject() {
       }
       data.append("physicalEducationRange", "100");
       data.append("daysLeft", "Awaiting Start");
-
-      if (selectedFil) {
-        data.append("attachment", selectedFil);
-      }
 
       requestData = data;
     } else {
@@ -543,10 +542,10 @@ export default function EditProject() {
             </div>
             <div className="mb-4">
               <label className="block text-sm font-semibold mb-2 text-gray-700">
-                {t("Attachment")}
+                {t("Attachments")}
               </label>
               <Controller
-                name="attachment"
+                name="attachments"
                 control={control}
                 render={({ field }) => (
                   <input
@@ -560,9 +559,9 @@ export default function EditProject() {
                   />
                 )}
               />
-              {errors.attachment && (
+              {errors.attachments && (
                 <span className="text-red-600">
-                  {errors.attachment.message}
+                  {errors.attachments.message}
                 </span>
               )}
             </div>
