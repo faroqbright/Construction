290fa9fe219417f1670af3113815b363d5622d0c
Awais-789 <awais.ranjha789@gmail.com>
2025-05-02 11:57:52 +0500

    CHanges

--- src/components/LandingPage/Client/ClientEvaluation.jsx
+++ src/components/LandingPage/Client/ClientEvaluation.jsx
@@ -70,7 +70,6 @@ const ClientEvaluation = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  console.log("Projects are:", projectsData);
   const fetchReviewsAndProcessProjects = useCallback(async () => {
     try {
       setLoading(true);
--- src/components/LandingPage/EditProject/EditProject.jsx
+++ src/components/LandingPage/EditProject/EditProject.jsx
@@ -985,6 +985,7 @@ export default function EditProject() {
             render={({ field }) => (
               <input
                 {...field}
+                type="text"
                 className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none"
               />
             )}
